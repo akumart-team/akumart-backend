e350b789c2507b1444e2b614f2ea6df5b18d27f9
Hilda-Enyioko <enyiokohilda@gmail.com>
2026-06-10 18:27:08 +0100

    fix: Import User table to be migrated on supabase

--- alembic/env.py
+++ alembic/env.py
@@ -11,6 +11,7 @@ from alembic import context
 
 from app.core.config import settings
 from app.core.database import Base
+from app.models.user import User     # pylint: disable=unused-import
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
